809be9d46824838abbc343614ac16e0b31b0fbe6
ElsaV22 <elsa22@att.net>
2020-07-23 00:06:12 -0500

    first part of SQL challenges

---
+++ SQLChallenges.sql
@@ -0,0 +1,65 @@
+-- find all products
+SELECT * FROM PRODUCTS;
+
+-- find all products that cost $1400
+SELECT * FROM products
+where Price = 1400;
+
+-- find all products that cost 11.99 or 13.99
+SELECT * FROM products
+where Price = '11.99' or Price = '13.99';
+
+-- find all products that do not cost 11.99 - using NOT
+SELECT * FROM products
+WHERE NOT price = '11.99';
+
+-- find all products and sort them by price from greatest to least
+SELECT * FROM products
+ORDER BY price DESC;
+
+-- find all employees who don't have a middle initial
+SELECT * FROM employees
+WHERE MiddleInitial IS NULL;
+
+-- find distinct product prices
+SELECT DISTINCT price FROM products;
+
+-- find all employees whose first name starts with the letter'j'
+SELECT * FROM employees 
+WHERE FirstName LIKE 'J%';
+
+-- find all Macbooks
+SELECT * FROM products
+WHERE Name = 'Macbook';
+
+-- find all products that are on sale
+SELECT * FROM products
+WHERE OnSale = 1;
+
+-- find the average price of all products
+SELECT AVG(price) FROM products;
+
+-- find all GeekSquad employees who don't have a middle initial
+SELECT * FROM employees 
+WHERE Title = 'Geek Squad' AND MiddleInitial IS NULL;
+
+-- find all products from products table whose stock level is in the range of 500 to 1200. order least to greatest. try between keyword
+SELECT * FROM products
+WHERE StockLevel BETWEEN 500 AND 1200
+ORDER BY StockLevel ASC;
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
